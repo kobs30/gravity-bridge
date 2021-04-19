c883495957745e6c7c38a6d3d94c9493953bcc8c
divined <divined@yandex.ru>
2021-04-19 15:30:18 +0200

    Update run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,5 +2,5 @@
 TEST_TYPE=$1
 set -eu
 
-# Run test entry point script
+# Run test entry point script test
 docker exec gravity_test_instance /bin/sh -c "pushd /gravity/ && tests/container-scripts/integration-tests.sh 1 $TEST_TYPE"
